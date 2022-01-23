57998416f10abdffa27acffb66ad338ba0c944a9
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2022-01-23 05:44:29 +0100

    Add test file

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use serde::Deserialize;
 
 #[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+#[serde(default)]
 pub struct Qua {
     /// The name of the audio file
     pub audio_file: String,
@@ -11,9 +13,9 @@ pub struct Qua {
     /// The name of the mapset banner
     pub banner_file: String,
     /// The unique Map Identifier (-1 if not submitted)
-    pub map_id: i32, // = -1
+    pub map_id: i32,
     /// The unique Map Set identifier (-1 if not submitted)
-    pub map_set_id: i32, // = -1
+    pub map_set_id: i32,
     /// The game mode for this map
     pub game_mode: GameMode,
     /// The title of the song
@@ -55,7 +57,7 @@ pub struct Qua {
     ///
     /// Note that SVs can be both in normalized and denormalized form, depending on BPMDoesNotAffectSV.
     /// Check WithNormalizedSVs if you need normalized SVs.
-    pub slider_velocities: Vec<SliderVelocityInfo>,
+    pub slider_velocities: Vec<ScrollVelocityInfo>,
     /// HitObject .qua data
     pub hit_objects: Vec<HitObjectInfo>,
 }
@@ -67,6 +69,37 @@ impl Qua {
     {
         let qua: Qua = serde_yaml::from_reader(reader)?;
         Ok(qua)
+    }
+}
+
+impl Default for Qua {
+    fn default() -> Self {
+        Self {
+            audio_file: "".to_string(),
+            song_preview_time: 0,
+            background_file: "".to_string(),
+            banner_file: "".to_string(),
+            map_id: -1,
+            map_set_id: -1,
+            game_mode: GameMode::Keys4,
+            title: "".to_string(),
+            artist: "".to_string(),
+            source: "".to_string(),
+            tags: "".to_string(),
+            creator: "".to_string(),
+            difficulty_name: "".to_string(),
+            description: "".to_string(),
+            genre: "".to_string(),
+            bpm_does_not_affect_scroll_velocity: false,
+            initial_scroll_velocity: 1.0,
+            has_scratch_key: false,
+            editor_layers: Vec::new(),
+            custom_audio_samples: Vec::new(),
+            sound_effects: Vec::new(),
+            timing_points: Vec::new(),
+            slider_velocities: Vec::new(),
+            hit_objects: Vec::new(),
+        }
     }
 }
 
@@ -94,17 +127,31 @@ impl GameMode {
 }
 
 #[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+#[serde(default)]
 pub struct EditorLayerInfo {
     /// The name of the layer
     pub name: String,
     /// Is the layer hidden in the editor?
     pub hidden: bool,
-    /// The color of the layer (default is white)
+    /// The color of the layer (default is white) in rrr,ggg,bbb format
     pub color_rgb: String,
+}
+
+impl Default for EditorLayerInfo {
+    fn default() -> Self {
+        Self {
+            name: "".to_string(),
+            hidden: false,
+            color_rgb: "255,255,255".to_string(),
+        }
+    }
 }
 
 /// CustomAudioSamples section of the .qua
 #[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+#[serde(default)]
 pub struct CustomAudioSampleInfo {
     /// The path to the audio sample.
     pub path: String,
@@ -112,7 +159,18 @@ pub struct CustomAudioSampleInfo {
     pub unaffected_by_rate: bool,
 }
 
+impl Default for CustomAudioSampleInfo {
+    fn default() -> Self {
+        Self {
+            path: "".to_string(),
+            unaffected_by_rate: false,
+        }
+    }
+}
+
 #[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+#[serde(default)]
 pub struct SoundEffectInfo {
     /// The time at which to play the sound sample.
     pub start_time: f32,
@@ -122,26 +180,50 @@ pub struct SoundEffectInfo {
     pub volume: i32,
 }
 
+impl Default for SoundEffectInfo {
+    fn default() -> Self {
+        Self {
+            start_time: 0.0,
+            sample: 0,
+            volume: 0,
+        }
+    }
+}
+
 /// TimingPoints section of the .qua
 #[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+#[serde(default)]
 pub struct TimingPointInfo {
     /// The time in milliseconds for when this timing point begins
     pub start_time: i32,
     /// The BPM during this timing point
     pub bpm: f32,
-}
-
-/// SliderVelocities section of the .qua
-#[derive(Deserialize)]
-pub struct SliderVelocityInfo {
-    /// The time in milliseconds when the new SliderVelocity section begins
-    pub start_time: i32,
-    /// The velocity multiplier relative to the current timing section's BPM
-    pub multiplier: f32,
     /// The signature during this timing point
     pub signature: TimeSignature,
     /// Whether timing lines during this timing point should be hidden or not
     pub hidden: bool,
+}
+
+impl Default for TimingPointInfo {
+    fn default() -> Self {
+        Self {
+            start_time: 0,
+            bpm: 0.0,
+            signature: TimeSignature::Quadruple,
+            hidden: false,
+        }
+    }
+}
+
+/// SliderVelocities section of the .qua
+#[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct ScrollVelocityInfo {
+    /// The time in milliseconds when the new SliderVelocity section begins
+    pub start_time: i32,
+    /// The velocity multiplier relative to the current timing section's BPM
+    pub multiplier: f32,
 }
 
 #[derive(Deserialize)]
@@ -151,6 +233,8 @@ pub enum TimeSignature {
 }
 
 #[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+#[serde(default)]
 pub struct HitObjectInfo {
     /// The time in milliseconds when the HitObject is supposed to be hit.
     pub start_time: i32,
@@ -159,24 +243,69 @@ pub struct HitObjectInfo {
     /// The endtime of the HitObject (if greater than 0, it's considered a hold note.)
     pub end_time: i32,
     /// Bitwise combination of hit sounds for this object
-    pub hit_sound: HitSounds,
+    // TODO: Handle hitsound bitflags
+    pub hit_sound: u8,
     /// Key sounds to play when this object is hit.
     pub key_sounds: Vec<KeySoundInfo>,
     /// The layer in the editor that the object belongs to.
     pub editor_layer: i32,
 }
 
-#[derive(Deserialize)]
-pub enum HitSounds {
-    Normal = 1 << 0,  // This is 1, but Normal should be played regardless if it's 0 or 1.
-    Whistle = 1 << 1, // 2
-    Finish = 1 << 2,  // 4
-    Clap = 1 << 3,    // 8
+impl Default for HitObjectInfo {
+    fn default() -> Self {
+        Self {
+            start_time: 0,
+            lane: 1,
+            end_time: 0,
+            hit_sound: 0,
+            key_sounds: Vec::new(),
+            editor_layer: 0,
+        }
+    }
 }
 
 /// KeySounds property of hit objects.
 #[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+#[serde(default)]
 pub struct KeySoundInfo {
     pub sample: i32,
     pub volume: i32,
+}
+
+impl Default for KeySoundInfo {
+    fn default() -> Self {
+        Self {
+            sample: 0,
+            volume: 100,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{fs::File, path::Path};
+
+    #[test]
+    fn test_read() {
+        let path = Path::new("./map_files/1416.qua");
+        let file = File::open(path).expect("Could not read file");
+        let qua = Qua::from_reader(file).expect("Could not parse qua");
+
+        assert_eq!("Csikos Post", qua.title);
+        assert_eq!("zetoban", qua.artist);
+        assert_eq!(1416, qua.map_id);
+
+        assert_eq!(1, qua.timing_points.len());
+        assert_eq!(0, qua.timing_points[0].start_time); // Default value
+
+        assert_eq!(167, qua.hit_objects.len());
+        assert_eq!(0, qua.hit_objects[0].start_time); // Default value
+        assert_eq!(1, qua.hit_objects[0].lane);
+
+        assert!(!qua.bpm_does_not_affect_scroll_velocity); // Default value
+
+        assert_eq!(4, qua.game_mode.get_key_count());
+    }
 }

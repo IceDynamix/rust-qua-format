9ca37a9653f4bb7161c86dfc9b9db56aee31a666
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2022-01-23 05:01:16 +0100

    Add serde deserializing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
 pub struct Qua {
     /// The name of the audio file
     pub audio_file: String,
@@ -57,11 +60,40 @@ pub struct Qua {
     pub hit_objects: Vec<HitObjectInfo>,
 }
 
+impl Qua {
+    fn from_reader<R>(reader: R) -> Result<Qua, serde_yaml::Error>
+    where
+        R: std::io::Read,
+    {
+        let qua: Qua = serde_yaml::from_reader(reader)?;
+        Ok(qua)
+    }
+}
+
+#[derive(Deserialize)]
 pub enum GameMode {
     Keys4 = 1,
     Keys7 = 2,
 }
 
+impl GameMode {
+    fn from_key_count(key_count: i32) -> Option<GameMode> {
+        match key_count {
+            4 => Some(GameMode::Keys4),
+            7 => Some(GameMode::Keys7),
+            _ => None,
+        }
+    }
+
+    fn get_key_count(self) -> i32 {
+        match self {
+            GameMode::Keys4 => 4,
+            GameMode::Keys7 => 7,
+        }
+    }
+}
+
+#[derive(Deserialize)]
 pub struct EditorLayerInfo {
     /// The name of the layer
     pub name: String,
@@ -72,6 +104,7 @@ pub struct EditorLayerInfo {
 }
 
 /// CustomAudioSamples section of the .qua
+#[derive(Deserialize)]
 pub struct CustomAudioSampleInfo {
     /// The path to the audio sample.
     pub path: String,
@@ -79,6 +112,7 @@ pub struct CustomAudioSampleInfo {
     pub unaffected_by_rate: bool,
 }
 
+#[derive(Deserialize)]
 pub struct SoundEffectInfo {
     /// The time at which to play the sound sample.
     pub start_time: f32,
@@ -89,6 +123,7 @@ pub struct SoundEffectInfo {
 }
 
 /// TimingPoints section of the .qua
+#[derive(Deserialize)]
 pub struct TimingPointInfo {
     /// The time in milliseconds for when this timing point begins
     pub start_time: i32,
@@ -97,6 +132,7 @@ pub struct TimingPointInfo {
 }
 
 /// SliderVelocities section of the .qua
+#[derive(Deserialize)]
 pub struct SliderVelocityInfo {
     /// The time in milliseconds when the new SliderVelocity section begins
     pub start_time: i32,
@@ -108,11 +144,13 @@ pub struct SliderVelocityInfo {
     pub hidden: bool,
 }
 
+#[derive(Deserialize)]
 pub enum TimeSignature {
     Quadruple = 4,
     Triple = 3,
 }
 
+#[derive(Deserialize)]
 pub struct HitObjectInfo {
     /// The time in milliseconds when the HitObject is supposed to be hit.
     pub start_time: i32,
@@ -128,6 +166,7 @@ pub struct HitObjectInfo {
     pub editor_layer: i32,
 }
 
+#[derive(Deserialize)]
 pub enum HitSounds {
     Normal = 1 << 0,  // This is 1, but Normal should be played regardless if it's 0 or 1.
     Whistle = 1 << 1, // 2
@@ -136,6 +175,7 @@ pub enum HitSounds {
 }
 
 /// KeySounds property of hit objects.
+#[derive(Deserialize)]
 pub struct KeySoundInfo {
     pub sample: i32,
     pub volume: i32,
